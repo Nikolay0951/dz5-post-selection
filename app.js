c9eefab7a5a4e48dc4e458fca810d45377efd929
Nikolay0951 <egnicholas333@gmail.com>
2019-10-23 22:41:01 +0300

    Добавил лайки/дизлайки

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const links = [];
 
 const addFormEl = document.createElement('form');
 addFormEl.innerHTML = `
-    <input data-id="link">
+<div class="form1">
+    <input data-id="link" placeholder="Введите тест или url источника" size="30">
+</div>
     <select data-id="type">
         <option value="regular">Обычный</option>
         <option value="image">Изображение</option>
         <option value="audio">Аудио</option>
         <option value="video">Видео</option>
     </select>
-    <button>Ok</button>
+    <button class="btn btn-primary">Добавить</button>
 `;
 
 const linkEl = addFormEl.querySelector('[data-id=link]');
@@ -26,10 +28,12 @@ addFormEl.onsubmit = function (ev) {
     console.dir(linkEl);
     const value = linkEl.value;
     const type = typeEl.value;
+
     console.log(type);
     links.push({
         value,
         type,
+        likes: 0,
     });
     console.log(links);
     linkEl.value = '';
@@ -46,11 +50,14 @@ function rebuildList(containerEl, items) {
     for (const item of items) {
         const liEl = document.createElement('div');
         liEl.className = 'card-posts';
+
         if (item.type === 'regular') {
             liEl.innerHTML = `
             <div class="card">
                 <div class="card-body">
                     <p class="card-text">${item.value}</p>
+                    <button class="btn btn-primary" data-action="like">👍🏼: ${item.likes}</button>
+                    <button class="btn btn-secondary" data-action="dislike">👎🏻</button>
                 </div>
             </div>
             `;
@@ -58,14 +65,21 @@ function rebuildList(containerEl, items) {
             liEl.innerHTML = `
             <div class="card">
                 <img src="${item.value}" class="card-img-top">
-                <div class="card-body"></div>
+                <div class="card-body">
+                    <button class="btn btn-primary" data-action="like">👍🏼: ${item.likes}</button>
+                    <button class="btn btn-secondary" data-action="dislike">👎🏻</button>
+                </div>
             </div>
                 `;
         } else if (item.type === 'video') {
             liEl.innerHTML = `
             <div class="card">
                 <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="${item.value}"></iframe>
+                     <iframe class="embed-responsive-item" src="${item.value}"></iframe>
+                </div>
+                <div class="card-body">
+                    <button class="btn btn-primary" data-action="like">👍🏼: ${item.likes}</button>
+                    <button class="btn btn-secondary" data-action="dislike">👎🏻</button>
                 </div>
             </div>
             `;
@@ -75,13 +89,28 @@ function rebuildList(containerEl, items) {
                 <div class="card-img-top">
                     <audio src="${item.value}" controls></audio>
                 </div>
+                <div class="card-body">
+                    <button class="btn btn-primary" data-action="like">👍🏼: ${item.likes}</button>
+                    <button class="btn btn-secondary" data-action="dislike">👎🏻</button>
+                </div>
             </div>
             `;
         }
         liEl.onclick = function () {
             item.done = !item.done;
             rebuildList(containerEl, items);
-        }
+        };
+        const likeEl = liEl.querySelector('[data-action=like]');
+        likeEl.onclick = function () {
+            item.likes++;
+            rebuildTree(containerEl, items);
+        };
+
+        const dislikeEl = liEl.querySelector('[data-action=dislike]');
+        dislikeEl.onclick = function () {
+            item.likes--;
+            rebuildTree(containerEl, items);
+        };
         containerEl.appendChild(liEl);
     }
 }
